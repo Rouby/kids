6ba4da45866386c280cc5e9609d58d570b3bf0d9
Jonathan Burke <jonathan.burke.1311@googlemail.com>
2023-12-02 17:05:38 +0100

    feat: add simple game loop
    Signed-off-by: Jonathan Burke <jonathan.burke.1311@googlemail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,148 @@
+import { randomId } from '@mantine/hooks';
 import { motion, useMotionValue, useSpring } from 'framer-motion';
-import { useEffect } from 'react';
+import { RefObject, createRef, useEffect, useState } from 'react';
 
 export function App() {
+  const [score, setScore] = useState(0);
+
+  const [planets, setPlanets] = useState<
+    { id: string; ref: RefObject<HTMLDivElement>; initialX: number }[]
+  >([
+    {
+      id: randomId(),
+      ref: createRef(),
+      initialX: Math.random() * document.body.clientWidth,
+    },
+  ]);
+  const [coins, setCoins] = useState<
+    { id: string; ref: RefObject<HTMLDivElement>; initialX: number }[]
+  >([]);
+
+  useEffect(() => {
+    for (const _ of [0]) {
+      setTimeout(() => {
+        setPlanets((planets) => [
+          ...planets,
+          {
+            id: randomId(),
+            ref: createRef(),
+            initialX: Math.random() * document.body.clientWidth,
+          },
+        ]);
+      }, 250 + Math.random() * 1000);
+    }
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCoins((coins) =>
+        coins.length > 10
+          ? coins
+          : [
+              ...coins,
+              {
+                id: randomId(),
+                ref: createRef(),
+                initialX: Math.random() * document.body.clientWidth,
+              },
+            ],
+      );
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      planets.forEach((planet) => {
+        const planetRect = planet.ref.current?.getBoundingClientRect();
+        const rocketRect = document
+          .querySelector('[style*="pointer-events: none"]')
+          ?.getBoundingClientRect();
+
+        if (
+          planetRect &&
+          rocketRect &&
+          planetRect.left < rocketRect.right &&
+          planetRect.right > rocketRect.left &&
+          planetRect.top < rocketRect.bottom &&
+          planetRect.bottom > rocketRect.top
+        ) {
+          // setPlanets((planets) => planets.filter((p) => p.id !== planet.id));
+          setScore(0);
+        }
+      });
+
+      coins.forEach((coin) => {
+        const coinRect = coin.ref.current?.getBoundingClientRect();
+        const rocketRect = document
+          .querySelector('[style*="pointer-events: none"]')
+          ?.getBoundingClientRect();
+
+        if (
+          coinRect &&
+          rocketRect &&
+          coinRect.left < rocketRect.right &&
+          coinRect.right > rocketRect.left &&
+          coinRect.top < rocketRect.bottom &&
+          coinRect.bottom > rocketRect.top
+        ) {
+          setCoins((coins) => coins.filter((c) => c.id !== coin.id));
+          setScore((score) => score + 1);
+        }
+      });
+    }, 1);
+
+    return () => clearInterval(interval);
+  }, [coins]);
+
   return (
     <>
+      <Background />
+
+      <div
+        style={{
+          position: 'absolute',
+          top: '5vh',
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+          fontSize: 40,
+          color: 'yellow',
+        }}
+      >
+        {score}
+      </div>
+
       <Rocket />
 
-      <Planet />
+      {planets.map((planet) => (
+        <Planet
+          key={planet.id}
+          innerRef={planet.ref}
+          initialX={planet.initialX}
+        />
+      ))}
+
+      {coins.map((coin) => (
+        <Coin key={coin.id} innerRef={coin.ref} initialX={coin.initialX} />
+      ))}
     </>
+  );
+}
+
+function Background() {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        background: 'black',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+      }}
+    />
   );
 }
 
@@ -16,7 +151,7 @@ function Rocket() {
 
   const rocketWidth = 50;
   const xPadding = 50;
-  const yPadding = 50;
+  const yPadding = '15vh';
 
   return (
     <>
@@ -58,8 +193,14 @@ function Rocket() {
   );
 }
 
-function Planet() {
-  const x = useMotionValue(0);
+function Planet({
+  innerRef,
+  initialX,
+}: {
+  innerRef: RefObject<HTMLDivElement>;
+  initialX: number;
+}) {
+  const x = useMotionValue(initialX);
   const y = useMotionValue(0);
 
   const size = 80;
@@ -78,9 +219,50 @@ function Planet() {
 
   return (
     <motion.div
+      ref={innerRef}
       style={{
         position: 'absolute',
         background: 'red',
+        x,
+        y,
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+      }}
+    />
+  );
+}
+
+function Coin({
+  innerRef,
+  initialX,
+}: {
+  innerRef: RefObject<HTMLDivElement>;
+  initialX: number;
+}) {
+  const x = useMotionValue(initialX);
+  const y = useMotionValue(0);
+
+  const size = 40;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      y.set(y.get() + 2);
+      if (y.get() > document.body.clientHeight) {
+        x.set(Math.random() * document.body.clientWidth);
+        y.set(0);
+      }
+    }, 1);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <motion.div
+      ref={innerRef}
+      style={{
+        position: 'absolute',
+        background: 'yellow',
         x,
         y,
         width: size,
