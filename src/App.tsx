65b5e1bb099292623d4596b524dd627b80ced0d2
Jonathan Burke <jonathan.burke.1311@googlemail.com>
2023-12-02 16:42:25 +0100

    feat: add planet falling
    Signed-off-by: Jonathan Burke <jonathan.burke.1311@googlemail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import { motion, useSpring } from 'framer-motion';
+import { motion, useMotionValue, useSpring } from 'framer-motion';
+import { useEffect } from 'react';
 
 export function App() {
+  return (
+    <>
+      <Rocket />
+
+      <Planet />
+    </>
+  );
+}
+
+function Rocket() {
   const x = useSpring(0);
 
+  const rocketWidth = 50;
   const xPadding = 50;
   const yPadding = 50;
-  const rocketWidth = 50;
 
   return (
     <>
       <motion.div
         style={{
-          background: 'red',
           position: 'absolute',
+          background: 'red',
           pointerEvents: 'none',
           x,
           bottom: yPadding,
@@ -23,9 +34,9 @@ export function App() {
 
       <div
         style={{
+          position: 'absolute',
           background: 'transparent',
           touchAction: 'none',
-          position: 'absolute',
           top: 0,
           bottom: 0,
           left: 0,
@@ -44,5 +55,38 @@ export function App() {
         }}
       />
     </>
+  );
+}
+
+function Planet() {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  const size = 80;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      y.set(y.get() + 2);
+      if (y.get() > document.body.clientHeight) {
+        x.set(Math.random() * document.body.clientWidth);
+        y.set(0);
+      }
+    }, 1);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <motion.div
+      style={{
+        position: 'absolute',
+        background: 'red',
+        x,
+        y,
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+      }}
+    />
   );
 }
